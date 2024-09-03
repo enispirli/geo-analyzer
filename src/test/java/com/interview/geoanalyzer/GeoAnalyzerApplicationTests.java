1706911e1250d4237b01afaf05bcccd60e32c21d
enispirli <en.ispirli@gmail.com>
2024-09-03 19:18:23 +0300

    initial commit

---
+++ src/test/java/com/interview/geoanalyzer/GeoAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interview.geoanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
